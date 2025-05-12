d2e299e87bcf1db3e2ee5d01b44f5042db0d5a86
Sralud <llorente.gerald2@gmail.com>
2025-05-12 10:41:00 +0100

    update

--- flights-api/app/Models/FlightSearch.php
+++ flights-api/app/Models/FlightSearch.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FlightSearch extends Model
 {
-    //
+    protected $table = 'flight_searches';
+
+    // Fillable fields for mass assignment (like create(), update())
+    protected $fillable = [
+        'origin',
+        'destination',
+        'departure_date',
+    ];
 }
