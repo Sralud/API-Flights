4010550493f89b172f3135a2e9a89a3d5f45f1a3
Sralud <llorente.gerald2@gmail.com>
2025-05-13 13:11:00 +0100

    update

--- flights-api/routes/web.php
+++ flights-api/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FlightController;
+//use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\FlightController;
 
-Route::get('/flights', [FlightController::class, 'index']);
-Route::get('/flights/{id}', [FlightController::class, 'show']);
-Route::post('/flights', [FlightController::class, 'store']);
-Route::put('/flights/{id}', [FlightController::class, 'update']);
-Route::delete('/flights/{id}', [FlightController::class, 'destroy']);
-Route::get('/flights/search', [FlightController::class, 'search']);
+//Route::get('/flights', [FlightController::class, 'index']);
+//Route::get('/flights/{id}', [FlightController::class, 'show']);
+//Route::post('/flights', [FlightController::class, 'store']);
+//Route::put('/flights/{id}', [FlightController::class, 'update']);
+//Route::delete('/flights/{id}', [FlightController::class, 'destroy']);
+//Route::get('/flights/search', [FlightController::class, 'search']);
